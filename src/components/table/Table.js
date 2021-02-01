f71884feb0638754bbf6e9e9a6692ddda202bd17
Marina Ismailova <marina1988sherbakova@gmail.com>
2021-01-31 19:39:11 -0500

    resize-table: move Resize functionality to the new file

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
-import {$} from '@core/Dom';
+import {resizeHandler} from '@/components/table/table.resize';
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
   constructor($root) {
@@ -15,48 +16,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target);
-      const $parent = $resizer.closest('[data-type], resizable');
-      const coords = $parent.getCoords();
-      const currentColIndex = $parent.data.column;
-      const type = $resizer.data.resize;
-      const colToResize = this.$root.findAll(`[data-column="${currentColIndex}"]`);
-      const sideProp = type === 'column' ? 'bottom' : 'right';
-      let calcSize;
-
-      $resizer.css({
-        opacity: 1,
-        [sideProp]: '-2000px',
-      })
-
-      document.onmousemove = (e) => {
-        if (type === 'column') {
-          const delta = e.pageX - coords.right;
-          calcSize = `${coords.width + delta}px`;
-          $resizer.css({right: -delta + 'px'})
-        } else {
-          const delta = e.pageY - coords.bottom;
-          calcSize = `${coords.height + delta}px`;
-          $resizer.css({bottom: -delta + 'px'})
-        }
-      }
-
-      document.onmouseup = () => {
-        document.onmousemove = null;
-        document.onmouseup = null;
-        if (type === 'column') {
-          colToResize.forEach((col) => $(col).css({width: calcSize}));
-        } else {
-          $parent.css({height: calcSize});
-        }
-
-        $resizer.css({
-          bottom: 0,
-          opacity: 0,
-          right: 0,
-        })
-      };
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
     }
   }
 }
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,45 @@
+import {$} from '@core/Dom';
+
+export function resizeHandler($root, event) {
+  const $resizer = $(event.target);
+  const $parent = $resizer.closest('[data-type], resizable');
+  const coords = $parent.getCoords();
+  const currentColIndex = $parent.data.column;
+  const type = $resizer.data.resize;
+  const colToResize = $root.findAll(`[data-column="${currentColIndex}"]`);
+  const sideProp = type === 'column' ? 'bottom' : 'right';
+  let calcSize;
+
+  $resizer.css({
+    opacity: 1,
+    [sideProp]: '-2000px',
+  })
+
+  document.onmousemove = (e) => {
+    if (type === 'column') {
+      const delta = e.pageX - coords.right;
+      calcSize = `${coords.width + delta}px`;
+      $resizer.css({right: -delta + 'px'})
+    } else {
+      const delta = e.pageY - coords.bottom;
+      calcSize = `${coords.height + delta}px`;
+      $resizer.css({bottom: -delta + 'px'})
+    }
+  }
+
+  document.onmouseup = () => {
+    document.onmousemove = null;
+    document.onmouseup = null;
+    if (type === 'column') {
+      colToResize.forEach((col) => $(col).css({width: calcSize}));
+    } else {
+      $parent.css({height: calcSize});
+    }
+
+    $resizer.css({
+      bottom: 0,
+      opacity: 0,
+      right: 0,
+    })
+  };
+}
